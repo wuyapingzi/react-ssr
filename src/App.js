61565764101fc961808cdadc5b2ee3a5f048124e
wuyaping <wuyaping@licaigc.com>
2019-12-10 18:32:14 +0800

    react-ssr 同构实现累加器

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const App = (props) => {
+  const [count, setCount] = useState(1)
+  return (
+    <div>
+      <h1>hello {props.title} ! wuyaping {count}</h1>
+      <button
+        onClick={() => setCount(count + 1)}
+      >累加</button>
+    </div>
+  )
+}
+
+export default <App title='开课吧' />
---
+++ webpack.client.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  // 客户端入口
+  entry: "./client/index.js",
+  // 客户端输出
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules",
+        options: {
+          presets: ["@babel/preset-react", ["@babel/preset-env"]]
+        }
+      }
+    ]
+  }
+};
